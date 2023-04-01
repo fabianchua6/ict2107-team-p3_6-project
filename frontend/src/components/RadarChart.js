fd7bd5a05aa3ea951fec7819272b485dd1aa9c11
Fabian Chua <fabianchua6@gmail.com>
2023-04-01 20:09:12 +0800

    update dashboard and radarchart

--- frontend/src/components/RadarChart.js
+++ frontend/src/components/RadarChart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {
   Chart as ChartJS,
   RadialLinearScale,
@@ -29,41 +29,25 @@ function RadarChart({ selectedCompany }) {
     'Very Negative',
   ];
 
-  const companiesArray = Object.entries(companiesData).map(
-    ([company, data]) => ({
+  const companiesArray = useMemo(() => {
+    return Object.entries(companiesData).map(([company, data]) => ({
       company,
       breakdown: data['Breakdown of Sentiments'],
-      totalReviews: data['Number of Reviews Analysed'],
-    })
-  );
+    }));
+  }, [companiesData]);
 
-  const selectedCompanyData = companiesArray.find(
-    ({ company }) => company === selectedCompany
-  );
-  const selectedCompanyScores = {};
-  labels.forEach(label => {
-    selectedCompanyScores[label] =
-      selectedCompanyData.breakdown[label] / selectedCompanyData.totalReviews;
-  });
-
-  const otherCompaniesData = companiesArray
-    .filter(({ company }) => company !== selectedCompany)
-    .map(({ breakdown, totalReviews }) => {
-      const scores = {};
-      labels.forEach(label => {
-        scores[label] = breakdown[label] / totalReviews;
-      });
-      return scores;
-    });
-
-  const otherCompaniesAverage = {};
-  labels.forEach(label => {
-    const sum = otherCompaniesData.reduce(
-      (total, current) => total + current[label],
+  const totalReviews = labels.reduce((acc, label) => {
+    acc[label] = companiesArray.reduce(
+      (sum, { breakdown }) => sum + breakdown[label],
       0
     );
-    otherCompaniesAverage[label] = sum / otherCompaniesData.length;
-  });
+    return acc;
+  }, {});
+
+  const averageReviews = {};
+  for (const label of labels) {
+    averageReviews[label] = totalReviews[label] / companiesArray.length;
+  }
 
   const chartOptions = {
     responsive: true,
@@ -73,7 +57,7 @@ function RadarChart({ selectedCompany }) {
       },
       title: {
         display: true,
-        text: `Sentiment Breakdown: ${selectedCompany} vs. Others`,
+        text: `Number of Reviews: ${selectedCompany} vs. Others`,
       },
     },
     scales: {
@@ -89,25 +73,61 @@ function RadarChart({ selectedCompany }) {
     },
   };
 
-  const chartDataConfig = {
+  const [chartDataConfig, setChartDataConfig] = useState({
     labels: labels,
     datasets: [
       {
         label: selectedCompany,
-        data: Object.values(selectedCompanyScores),
+        data: [],
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
       },
       {
         label: 'Other Companies',
-        data: Object.values(otherCompaniesAverage),
+        data: [],
         backgroundColor: 'rgba(54, 162, 235, 0.2)',
         borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1,
       },
     ],
-  };
+  });
+
+  useEffect(() => {
+    const selectedCompanyData = companiesArray.find(
+      ({ company }) => company === selectedCompany
+    );
+
+    const totalReviews = labels.reduce((acc, label) => {
+      acc[label] = companiesArray.reduce(
+        (sum, { breakdown }) => sum + breakdown[label],
+        0
+      );
+      return acc;
+    }, {});
+
+    const averageReviews = {};
+    for (const label of labels) {
+      averageReviews[label] = totalReviews[label] / companiesArray.length;
+    }
+
+    const updatedChartDataConfig = {
+      ...chartDataConfig,
+      datasets: [
+        {
+          ...chartDataConfig.datasets[0],
+          label: selectedCompany,
+          data: Object.values(selectedCompanyData.breakdown),
+        },
+        {
+          ...chartDataConfig.datasets[1],
+          data: Object.values(averageReviews),
+        },
+      ],
+    };
+
+    setChartDataConfig(updatedChartDataConfig);
+  }, [selectedCompany, companiesArray]);
 
   return <Radar data={chartDataConfig} options={chartOptions} />;
 }
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -3,7 +3,10 @@ import { Flex, Grid } from '@chakra-ui/layout';
 import { Select } from '@chakra-ui/select';
 import { Stat, StatLabel, StatNumber } from '@chakra-ui/stat';
 import {
+  FormControl,
+  FormLabel,
   Heading,
+  Switch,
   Tab,
   TabList,
   TabPanel,
@@ -14,24 +17,28 @@ import React, { useState, useEffect } from 'react';
 import RadarChart from '../components/RadarChart';
 import PolarPie from '../components/PolarPie';
 import jsonData from '../wordMapFinalOutput.json';
+import NLPJsonData from '../sampleNlpSaOutput.json';
 import WordCloud from '../pages/WordCloud';
 import PieChart from '../components/PieChart';
 import { ScatterChart } from '../components/ScatterChart';
 
-const CardStat = ({ title, numbers, color }) => {
+const CardStat = ({ title, numbers, total, color }) => {
+  const percentage = total ? ((numbers / total) * 100).toFixed(2) + '%' : '-';
   return (
     <Card bg={color} w="full" h="full">
       <CardBody>
         <Stat>
           <StatLabel>{title}</StatLabel>
-          <StatNumber>{numbers}</StatNumber>
+          <StatNumber>
+            {numbers} ({percentage})
+          </StatNumber>
         </Stat>
       </CardBody>
     </Card>
   );
 };
 
-const SentimentStat = ({ display, title, numbers }) => {
+const SentimentStat = ({ display, title, numbers, total }) => {
   return (
     <Card display={display}>
       <CardBody>
@@ -47,12 +54,22 @@ const SentimentStat = ({ display, title, numbers }) => {
 export default function Dashboard() {
   const [selectedCompany, setSelectedCompany] = useState('DBS');
   const [sentimentData, setSentimentData] = useState(null);
+  const [useNLPData, setUseNLPData] = useState(false);
+  const handleUseNLPDataChange = event => {
+    setUseNLPData(event.target.checked);
+  };
+  const totalReviews = sentimentData
+    ? sentimentData.reduce((a, b) => a + b, 0)
+    : null;
 
   useEffect(() => {
-    const breakdown = jsonData[selectedCompany]?.['Breakdown of Sentiments'];
+    const breakdown =
+      (useNLPData ? NLPJsonData : jsonData)[selectedCompany]?.[
+        'Breakdown of Sentiments'
+      ] ?? {};
 
     setSentimentData(Object.values(breakdown));
-  }, [selectedCompany]);
+  }, [selectedCompany, useNLPData]);
 
   const handleCompanyChange = event => {
     setSelectedCompany(event.target.value);
@@ -96,34 +113,42 @@ export default function Dashboard() {
         <CardStat
           title="Negative"
           numbers={sentimentData ? sentimentData[3] : '-'}
+          total={totalReviews}
           color="red.200"
         />
         <CardStat
           title="Very Negative"
           numbers={sentimentData ? sentimentData[4] : '-'}
+          total={totalReviews}
           color="red.400"
         />
         <SentimentStat
           // hide in mobile view
           display={{ base: 'none', md: 'block' }}
           title={'Average Sentiment Score'}
+          //
           numbers={
-            jsonData[selectedCompany]?.['Average sentiment per review'] || '-'
+            useNLPData
+              ? '-'
+              : jsonData[selectedCompany]?.['Average sentiment per review']
           }
         />
         <CardStat
           title="Neutral"
           numbers={sentimentData ? sentimentData[2] : '-'}
+          total={totalReviews}
           color="gray.300"
         />
         <CardStat
           title="Positive"
           numbers={sentimentData ? sentimentData[1] : '-'}
+          total={totalReviews}
           color="green.200"
         />
         <CardStat
           title="Very Positive"
           numbers={sentimentData ? sentimentData[0] : '-'}
+          total={totalReviews}
           color="green.400"
         />
         <SentimentStat
@@ -131,25 +156,41 @@ export default function Dashboard() {
           display={{ base: 'block', md: 'none' }}
           title={'Average Sentiment Score'}
           numbers={
-            jsonData[selectedCompany]?.['Average sentiment per review'] || '-'
+            useNLPData
+              ? '-'
+              : jsonData[selectedCompany]?.['Average sentiment per review']
           }
         />
         <SentimentStat
           title={'Number of Reviews Analysed'}
           numbers={
-            jsonData[selectedCompany]?.['Number of Reviews Analysed'] || '-'
+            useNLPData
+              ? '-'
+              : jsonData[selectedCompany]?.['Number of Reviews Analysed']
           }
         />
       </Grid>
       <Flex direction={['column', 'row']} mt={4} gap={4}>
         <Card w={['full', '67%']} h="full">
           <CardHeader>
-            <Heading textAlign={'center'} size={'lg'}>
-              Sentimental Analysis of {selectedCompany}
-            </Heading>
+            <Flex>
+              <Heading w="full" textAlign={'center'} size={'lg'}>
+                Sentimental Analysis of {selectedCompany}
+              </Heading>
+              <FormControl
+                w="auto"
+                minWidth={'150px'}
+                display="flex"
+                direction="row"
+                alignItems="center"
+              >
+                <FormLabel mb="0">Use NLP Data</FormLabel>
+                <Switch onChange={handleUseNLPDataChange} />
+              </FormControl>
+            </Flex>
           </CardHeader>
           <CardBody>
-            <Tabs>
+            <Tabs isFitted>
               <TabList>
                 <Tab>Pie Chart</Tab>
                 <Tab>Polar Pie</Tab>
@@ -161,15 +202,12 @@ export default function Dashboard() {
                   <PieChart data={sentimentData} />
                 </TabPanel>
                 <TabPanel>
-                  <PolarPie
-                    data={sentimentData}
-                    title={`${selectedCompany} Sentiment Analysis`}
-                  />
+                  <PolarPie data={sentimentData} />
                 </TabPanel>
                 <TabPanel>
                   <RadarChart
-                    selectedCompany={selectedCompany}
                     data={sentimentData}
+                    selectedCompany={selectedCompany}
                   />
                 </TabPanel>
                 <TabPanel>
