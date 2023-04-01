4a8a40180962c29b51c22f18179d54e8e3aa9733
Fabian Chua <fabianchua6@gmail.com>
2023-04-01 18:29:29 +0800

    update word cloud with numbers

--- frontend/src/pages/WordCloud.js
+++ frontend/src/pages/WordCloud.js
@@ -11,36 +11,58 @@ function WordCloud({ selectedCompany }) {
 
   useEffect(() => {
     if (selectedCompany) {
-      const companyData = companiesData.find(
-        company => company.company === selectedCompany
-      );
-      setProsWords(companyData.pros.map(word => ({ text: word, value: 1 })));
-      setConsWords(companyData.cons.map(word => ({ text: word, value: 1 })));
+      const companyData = companiesData[selectedCompany];
+
+      // Add this check
+      if (companyData) {
+        setProsWords(
+          Object.entries(companyData['Pros Topic']).map(([text, value]) => ({
+            text,
+            value,
+          }))
+        );
+        setConsWords(
+          Object.entries(companyData['Cons Topic']).map(([text, value]) => ({
+            text,
+            value,
+          }))
+        );
+      }
     }
   }, [selectedCompany]);
 
+  const wordcloudSize = [200, 320]; // Set the size for both word clouds
+
   const proOptions = {
-    // shades of green
     colors: ['#99c68e', '#4DBD33', '#348017', '#96E97B', '#6AA84F'],
+    rotationAngles: [0, 45],
+    rotations: 0,
   };
 
   const consOptions = {
-    // shades of red
     colors: ['#FF9999', '#FF4F4F', '#FF1A1A', '#C11B17', '#E34234'],
+    rotationAngles: [0, 45],
+    rotations: 0,
   };
+
   return (
     <Box>
-      <Flex direction={'column'}>
-        <Heading fontSize={'xl'} textAlign={'center'}>
-          Pros
-        </Heading>
-        <ReactWordcloud options={proOptions} words={prosWords} />
-
-        <Heading fontSize={'xl'} textAlign={'center'}>
-          Cons
-        </Heading>
-        <ReactWordcloud options={consOptions} words={consWords} />
-      </Flex>
+      <Heading fontSize={'xl'} textAlign={'center'}>
+        Pros
+      </Heading>
+      <ReactWordcloud
+        options={proOptions}
+        size={wordcloudSize}
+        words={prosWords}
+      />
+      <Heading fontSize={'xl'} textAlign={'center'}>
+        Cons
+      </Heading>
+      <ReactWordcloud
+        options={consOptions}
+        size={wordcloudSize}
+        words={consWords}
+      />
     </Box>
   );
 }
