3b585ccca15162071b9f65622d17cfe5c7d6afe0
Fabian Chua <fabianchua6@gmail.com>
2023-04-02 15:22:45 +0800

    minor refactor

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,24 +1,20 @@
 import {
   Box,
-  chakra,
   Container,
   Stack,
   Text,
   Image,
   Flex,
   VStack,
-  Button,
   Heading,
   SimpleGrid,
   StackDivider,
   useColorModeValue,
-  VisuallyHidden,
   List,
   ListItem,
   ListIcon,
 } from '@chakra-ui/react';
-import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
-import { MdLocalShipping } from 'react-icons/md';
+
 import { CheckCircleIcon } from '@chakra-ui/icons';
 
 export default function About() {
